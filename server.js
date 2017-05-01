07e0259660ece643c426cc9d36648bc405506711
chrisisler <christopherisler1@gmail.com>
2017-05-01 01:38:16 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    const date = req.params[0].slice(1);
+    const isUnix = {}.toString.call(date) === '[object Number]';
+    const obj = Object.freeze({
+        unix: isUnix ? date : Date.parse(date),
+        natural: isUnix ? Math.floor(date / 1000) : date.replace(/%20/g, ' ')
+    });
+
+    res.send(JSON.stringify(obj));
 });
 
-app.listen((process.env.PORT || 3000), () => {
-    console.log('Example app listening on port 3000!');
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
 });
